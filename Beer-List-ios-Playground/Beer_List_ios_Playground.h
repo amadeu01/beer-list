088617f838d3bb7f4162e484a77e74873e1c6736
Amadeu Cavalcante Filho <amadeu01@gmail.com>
2017-12-09 21:27:34 -0200

    Added Playground workflow

---
+++ Beer-List-ios-Playground/Beer_List_ios_Playground.h
@@ -0,0 +1,19 @@
+//
+//  Beer_List_ios_Playground.h
+//  Beer-List-ios-Playground
+//
+//  Created by Amadeu Cavalcante Filho on 12/9/17.
+//  Copyright © 2017 Amadeu Cavalcante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Beer_List_ios_Playground.
+FOUNDATION_EXPORT double Beer_List_ios_PlaygroundVersionNumber;
+
+//! Project version string for Beer_List_ios_Playground.
+FOUNDATION_EXPORT const unsigned char Beer_List_ios_PlaygroundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Beer_List_ios_Playground/PublicHeader.h>
+
+
